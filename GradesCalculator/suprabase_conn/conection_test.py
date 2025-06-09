66de6ca1bfdf0124a05c4b914ee3b141f304f18e
Ernesto Rivera <futbolito2116@gmail.com>
2025-06-09 01:01:40 -0500

    adicion de un ejemplo de insert

--- GradesCalculator/suprabase_conn/conection_test.py
+++ GradesCalculator/suprabase_conn/conection_test.py
@@ -11,6 +11,8 @@ print(f"Key: {key}")
 
 supabase: Client = create_client(url, key)
 
+
+response = supabase.table("MATERIA").upsert({"N_MATERIA":"politica", "PARC_PORC": 30, "ASIG_PORC": 20, "ASIST_PORC": 5, "SEMESTRAL_PORC": 35, "CORREO_PROFESOR": "epa@gmail.com" , "CORREO_ELECTRONICO": "un_correo@gmail.com" }).execute()
 response = supabase.table("MATERIA").select("*").execute()
 
 print(response)
