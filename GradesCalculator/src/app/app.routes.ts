c3c0198ca6234c579e26fbc970cac9791de27b4f
Fobos <fabriciomendoza687@gmail.com>
2025-07-28 22:05:45 -0500

    añadir funcion de visualizar notas

--- GradesCalculator/src/app/app.routes.ts
+++ GradesCalculator/src/app/app.routes.ts
@@ -3,12 +3,15 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MateriasComponent } from './materias/materias.component';
+import { VisualizarNotasComponent } from './visualizar-notas/visualizar-notas.component';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: '/signup', pathMatch: 'full' },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'materias', component: MateriasComponent }, 
+  { path: 'materias', component: MateriasComponent },
+  { path: 'visualizar-notas', component: VisualizarNotasComponent }, 
   { path: '**', redirectTo: '/signup' }               
 ];
